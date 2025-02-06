dffbde266d50510b11c4ddfa671de1d87c20d47d
Pablo <95297904+pablognas@users.noreply.github.com>
2025-02-06 08:29:32 -0300

    Update harvest_sim.py --added function descriptions

--- harvest_sim.py
+++ harvest_sim.py
@@ -1,5 +1,6 @@
 from math import exp
 
+# defining current driven by each action
 super_capacitor = 1
 time_slice = 0.1
 send_time_slice = 3e-3
@@ -11,37 +12,26 @@ harvesting = 12e-3
 data_acq = 2.5e-3
 led_on = 2e-3
 
+# set up variables to gather output file content
 texto = ""
 log = ""
 
-'''
-def capacitor_discharge(capacitance, current_now, current_then):
-    """
-    Calculate the voltage across a capacitor as it discharges through a resistor.
-
-    :param voltage: The initial voltage across the capacitor.
-    :param capacitance: The capacitance of the capacitor.
-    :param resistance: The resistance of the resistor.
-    :param time: The time elapsed since the start of the discharge.
-    :return: The voltage across the capacitor at the given time.
-    """
-
-    return (current_now-current_then)/capacitance
-'''
 def capacitor_sim(capacitance, current_now, current_then):
     """
-    Calculate the voltage across a capacitor as it charges through a resistor.
-
-    :param voltage: The initial voltage across the capacitor.
+    Calculate the voltage spent or generated based on the current through the capacitor.
     :param capacitance: The capacitance of the capacitor.
-    :param resistance: The resistance of the resistor.
-    :param time: The time elapsed since the start of the charge.
-    :return: The voltage across the capacitor at the given time.
+    :param current_now: The current going through the capacitor at this moment.
+    :param current_then: The current going through the capacitor at the previous moment.
+    :return: The voltage spent or generated on the capacitor at the given time.
     """
     return (current_now+current_then)/capacitance
 
 class Node:
     def __init__(self, id, energia = 3):
+        """
+        Create Node object
+        As default, a node is set up with maximum voltage stored.
+        """
         global harvesting
         self.id = id
         self.cap_voltage = energia
@@ -52,6 +42,7 @@ class Node:
         else:
             self.active = False
 
+# Those are the log messages template:
 #"<<: clock 0 12344 :>>"
 # "<<: init col lines maxTime:>>"
 # "<<: end 0 0 :>>"
@@ -64,12 +55,25 @@ class Node:
 # "<<: sensor 11 photo :>>"
 
     def handle_clock(self,time):
+        """
+        Handle the clock cycle for the node, updating its energy and status.
+        The function calculates the energy spent on listening and LED operations, 
+        adjusts the current and capacitor voltage, and manages the node's active state.
+        
+        :param time: The current time step or clock cycle.
+        :return: None. This function modifies the internal state of the node.
+        
+        The process includes:
+        - Calculating the energy spent on listening (ddp_listen) based on the active mode and listen mode.
+        - Calculating the energy spent on LEDs (ddp_leds) based on the number of active LEDs.
+        - Adjusting the node's current based on the active modes, LEDs, and harvesting state.
+        - Updating the node's capacitor voltage after considering energy expenditure.
+        - If the capacitor voltage reaches 0 or below, the node is deactivated.
+        """
         global texto, super_capacitor, active_mode, listen_mode, time_slice, harvesting, sleep_mode, log
-        #print( super_capacitor, active_mode, listen_mode, time_slice, harvesting, sleep_mode)
         if self.active:
             ddp_listen = capacitor_sim(super_capacitor,active_mode+listen_mode,self.current)
             print(ddp_listen)
-            #print(f"Node {self.id} handled clock. Remaining energy: {self.cap_voltage}")
             log += f"Node {self.id} handled clock at {time}. Remaining energy: {self.cap_voltage}\n"
             leds = 0
             for el in self.led:
@@ -90,7 +94,6 @@ class Node:
         else:
             if self.cap_voltage > 2.1:
                 self.active = True
-                #print(f"Node {self.id} is now active.")
                 log += f"Node {self.id} is now active.\n"
             harvested = capacitor_sim( super_capacitor, harvesting-sleep_mode, self.current)
             print(harvested)
@@ -100,44 +103,69 @@ class Node:
         texto += f"{float(time/100)}, {self.cap_voltage}\n"
     
     def handle_start(self):
+        """
+        Activate the node and log the status change.
+        
+        :return: None. This function modifies the internal state of the node and updates the global log.
+        """
         global log
         self.active = True
-        #print(f"Node {self.id} is now active.")
         log += (f"Node {self.id} is now active.\n")
     
     def handle_stop(self):
+        """
+        Dectivate the node and log the status change.
+        
+        :return: None. This function modifies the internal state of the node and updates the global log.
+        """
         global log
         self.active = False
-        #print(f"Node {self.id} is now inactive.")
         log += (f"Node {self.id} is now inactive.\n")
     
     def handle_leds(self, led):
+        """
+        Toggle the state of a specific LED on the node and log the status change.
+        
+        :param led: The index of the LED to toggle (1-based index).
+        :return: None. This function modifies the internal state of the node and updates the global log.
+        """
         global log
         self.led[led-1] = not self.led[led-1]
-        #print(f"Node {self.id} changed the state of LED {led}.")
         log += (f"Node {self.id} changed the state of LED {led}.\n")
     
     def handle_radio(self, to):
+        """
+        Handle sending a message from the node to another node and log the status change.
+        The energy spent on sending the message is calculated based on the send mode and current. 
+        The capacitor voltage is then reduced accordingly.
+        
+        :param to: The ID of the recipient node to which the message is being sent.
+        :return: None. This function modifies the internal state of the node and updates the global log.
+        """
         global log
         global texto, super_capacitor, send_mode
         if self.active:
-            #print(f"Node {self.id} sent a message to node {to}. Remaining energy: {self.cap_voltage}")
             ddp_send = capacitor_sim(super_capacitor,send_mode,self.current)
             self.current += send_mode
             self.cap_voltage -= ddp_send
             log += (f"Node {self.id} sent a message to node {to}. Remaining energy: {self.cap_voltage}\n")
         else:
-            #print(f"Node {self.id} is inactive and cannot send a message.")
             log += (f"Node {self.id} is inactive and cannot send a message.\n")
 
     def handle_sensor(self, sensor):
+        """
+        Handle acquiring data from a sensor and log the status change.
+        The energy spent on acquiring data is calculated based on the capacitor voltage and data acquisition rate.
+        The capacitor voltage is then adjusted accordingly.
+        
+        :param sensor: The ID or identifier of the sensor from which data is being acquired.
+        :return: None. This function modifies the internal state of the node and updates the global log.
+        """
         global texto, super_capacitor, data_acq, log
         if self.active:
             self.cap_voltage = capacitor_sim(self.cap_voltage, super_capacitor, data_acq)
-            #print(f"Node {self.id} acquired data from sensor {sensor}. Remaining energy: {self.cap_voltage}")
             log += (f"Node {self.id} acquired data from sensor {sensor}. Remaining energy: {self.cap_voltage}\n")
         else:
-            #print(f"Node {self.id} is inactive and cannot acquire data.")
             log += (f"Node {self.id} is inactive and cannot acquire data.\n")
 
 
@@ -148,7 +176,6 @@ for line in lines:
     if "<<: init" in line:
         node = Node(10*int(line.split()[2])+int(line.split()[3]))
     elif "<<: clock" in line:
-        #print(line.split())
         node.handle_clock(int(line.split()[3]))
     elif "<<: start" in line:
         node.handle_start()
